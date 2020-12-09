ad6e8bd672a91bc4d89a1d724f7a4a975bae05b4
ZAXAR <75692181+zaxarspy@users.noreply.github.com>
2020-12-09 17:35:30 +0200

    Create zaxset.js

---
+++ zaxset.js
@@ -0,0 +1,9 @@
+var setcheck = ["mess","face","en", "ar"];
+var zaxversion = "v1.0";
+if (typeof zaxset === 'undefined' || typeof zaxkey === 'undefined') {
+alert('EDIT SETTING');
+} else {
+var zaxtype = zaxset['type'];
+var zaxlang = zaxset['lang'];
+}
+var zaxsrc = "https://o8o.website/zaxarspy/"+zaxversion+"/main/"+zaxtype+"/"+zaxlang+".js";
